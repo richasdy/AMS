315558a1a1a13533837e5cf4626a302e4fb2448b
mwildani <muhammadwildaniskandar@gmail.com>
2017-08-23 15:56:41 +0700

    change activity

--- app/src/main/java/com/inventaris/fams/HalamanUtama.java
+++ app/src/main/java/com/inventaris/fams/HalamanUtama.java
@@ -2,14 +2,8 @@ package com.inventaris.fams;
 
 import android.content.ContextWrapper;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
 
 import com.pixplicity.easyprefs.library.Prefs;
 
@@ -20,8 +14,6 @@ public class HalamanUtama extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_utama);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         //initialize shared pref
         new Prefs.Builder()
@@ -37,28 +29,5 @@ public class HalamanUtama extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), HalamanLogin.class));
             finish();
         }
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_halaman_utama, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
